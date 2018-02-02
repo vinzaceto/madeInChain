ff85f9875c53398a94ac21487b4e2713251206ea
Vince <vince.ajello@gmail.com>
2018-02-02 17:24:40 +0100

    Project init
    The creation of the project

---
+++ Waller/Waller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Waller
+//
+//  Created by Vincenzo Ajello on 02/02/18.
+//  Copyright © 2018 madeinchain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
