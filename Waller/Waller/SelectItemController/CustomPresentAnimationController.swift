9917eba62651d15e4dc6656b83f2b47afdba4803
Vincenzo Ajello <mskappa@gmail.com>
2018-02-11 18:19:31 +0100

    Latest

---
+++ Waller/Waller/SelectItemController/CustomPresentAnimationController.swift
@@ -0,0 +1,41 @@
+//
+//  CustomPresentAnimationController.swift
+//  SelectItemController
+//
+//  Created by keygx on 2017/03/14.
+//  Copyright © 2017年 keygx. All rights reserved.
+//
+
+import UIKit
+
+class CustomPresentAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+        
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to) as! SelectItemViewController
+        let containerView = transitionContext.containerView
+        containerView.addSubview(toViewController.view)
+        
+        toViewController.dialogView?.alpha = 0.2
+        toViewController.dialogView?.transform = CGAffineTransform(scaleX: 1.16, y: 1.16)
+        toViewController.effectView.alpha = 0.2
+        toViewController.effectView.transform = CGAffineTransform(scaleX: 1.16, y: 1.16)
+        
+        UIView.animate(withDuration: transitionDuration(using: transitionContext),
+                       delay: 0.0,
+                       options: [.curveEaseIn, .curveEaseOut],
+                       animations: {
+                toViewController.dialogView?.alpha = 1.0
+                toViewController.dialogView?.transform = CGAffineTransform.identity
+                toViewController.effectView.alpha = 1.0
+                toViewController.effectView.transform = CGAffineTransform.identity
+            
+            }, completion: { _ in
+                transitionContext.completeTransition(true)
+        })
+    }
+}
---
+++ Waller/Waller/SelectItemController/SelectItemController.swift
@@ -0,0 +1,102 @@
+//
+//  SelectItemController.swift
+//  SelectItemController
+//
+//  Created by keygx on 2017/03/14.
+//  Copyright © 2017年 keygx. All rights reserved.
+//
+
+import UIKit
+
+public struct Parameters {
+    public var title: String
+    public var itemTableView: ItemTableViewType?
+    public var items: [String]?
+    public var cancelButton: String
+    
+    public init(
+        title: String,
+        items: [String],
+        cancelButton: String
+        ) {
+        self.title = title
+        self.itemTableView = nil
+        self.items = items
+        self.cancelButton = cancelButton
+    }
+    
+    public init(
+        title: String,
+        itemTableView: ItemTableViewType,
+        cancelButton: String
+        ) {
+        self.title = title
+        self.itemTableView = itemTableView
+        self.items = nil
+        self.cancelButton = cancelButton
+    }
+}
+
+final public class SelectItemController: NSObject, SelectItemViewControllerDelegate, UIViewControllerTransitioningDelegate {
+    
+    private var completionHandler: ((Int?) -> Void)?
+    
+    let customPresentAnimationController = CustomPresentAnimationController()
+    
+    public override init() {
+        super.init()
+        completionHandler = nil
+    }
+    
+    public func show(parent: UIViewController, params: Parameters, completion: @escaping (Int?) -> Void) {
+        
+        let storyboard = UIStoryboard(name: "Dialog", bundle: Bundle(for: SelectItemViewController.self))
+        let selectItemViewController = storyboard.instantiateInitialViewController() as! SelectItemViewController
+        
+        // Title Label
+        selectItemViewController.dialogTitle = params.title
+        
+        // Button Label
+        selectItemViewController.buttonTitle = params.cancelButton
+        
+        // Table
+        selectItemViewController.itemTableView = params.itemTableView
+        
+        // Item
+        selectItemViewController.items = params.items
+        
+        // Handler
+        completionHandler = completion
+        
+        // Delegate
+        selectItemViewController.selectItemViewControllerDelegate = self
+        
+        // Style
+        selectItemViewController.modalPresentationStyle = .overCurrentContext
+        
+        // Transition
+        selectItemViewController.transitioningDelegate = self
+        
+        // Present Modal
+        parent.present(selectItemViewController, animated: true, completion: nil)
+    }
+    
+    func didSelected(index: Int?) {
+        // Callback
+        if let handler = completionHandler {
+            handler(index)
+        }
+        
+        defer {
+            completionHandler = nil
+        }
+    }
+    
+    public func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return customPresentAnimationController
+    }
+    
+    /*public func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return customPresentAnimationController
+    }*/
+}
