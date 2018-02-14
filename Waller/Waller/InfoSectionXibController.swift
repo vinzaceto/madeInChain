bf879b6e4727877a0c79aae5ee9ebcbbda9339ba
vincenzo aceto <vincenzo.aceto-apple@unina.it>
2018-02-14 18:04:18 +0100

    added timer in InfoSection

--- Waller/Waller/InfoSectionXibController.swift
+++ Waller/Waller/InfoSectionXibController.swift
@@ -16,30 +16,21 @@ class InfoSectionXibController: UIView {
     @IBOutlet weak var currentAmount: UILabel!
     @IBOutlet weak var currentBtcAmount: UILabel!
 
+    weak var timer: Timer?
+
     var contentView: UIView?
     @IBInspectable var nibName: String?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         xibSetup()
-        
+
         if let lastBtcValueTemp = UserDefaults.standard.string(forKey: Props.lastBtcValue) {
             self.currentBTCvalue.text = lastBtcValueTemp
         } else {
-            self.currentBTCvalue.text = "0$"
+            self.currentBTCvalue.text = "--"
         }
-        
-        let dataConnection = DataConnections()
-        dataConnection.getBitcoinValue(currency: Props.btcUsd) { (result) in
-            switch result {
-            case .success(let posts):
-                print(posts.last + "$")
-                UserDefaults.standard.set(posts.last + "$", forKey: Props.lastBtcValue)
-                self.currentBTCvalue.text = posts.last+"$"
-            case .failure(let error):
-                fatalError("error: \(error)")
-            }
-        }
+        startTimer()
     }
 
     func xibSetup() {
@@ -64,5 +55,32 @@ class InfoSectionXibController: UIView {
         super.prepareForInterfaceBuilder()
         xibSetup()
         contentView?.prepareForInterfaceBuilder()
+    }
+
+    func startTimer() {
+        timer?.invalidate() // just in case you had existing `Timer`, `invalidate` it before we lose our reference to it
+        timer = Timer.scheduledTimer(withTimeInterval: 60.0, repeats: true) { [weak self] _ in
+            let dataConnection = DataConnections()
+            dataConnection.getBitcoinValue(currency: Props.btcUsd) { (result) in
+                switch result {
+                case .success(let posts):
+                    print(posts.last + "$")
+                    UserDefaults.standard.set(posts.last + "$", forKey: Props.lastBtcValue)
+                    self?.currentBTCvalue.text = posts.last + "$"
+                case .failure(let error):
+                    fatalError("error: \(error)")
+                }
+            }
+        }
+    }
+
+    func stopTimer() {
+        timer?.invalidate()
+    }
+
+    // if appropriate, make sure to stop your timer in `deinit`
+
+    deinit {
+        stopTimer()
     }
 }
