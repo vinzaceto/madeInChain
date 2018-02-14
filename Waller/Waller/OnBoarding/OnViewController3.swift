5f4091388d90d650726ccc817fcba1f1df8042ba
Pasquale Mauriello <pasquale.ebasta@gmail.com>
2018-02-14 22:41:24 +0100

    Created a sample page on OnBoarding page one

--- Waller/Waller/OnBoarding/OnViewController3.swift
+++ Waller/Waller/OnBoarding/OnViewController3.swift
@@ -1,20 +1,28 @@
 //
-//  OnViewController.swift
+//  OnViewController3.swift
 //  Waller
 //
-//  Created by Pasquale Mauriello on 02/02/18.
+//  Created by Pasquale Mauriello on 14/02/18.
 //  Copyright © 2018 madeinchain. All rights reserved.
 //
 
 import UIKit
 
-class OnViewController: UIViewController, UIViewControllerTransitioningDelegate {
+class OnViewController3: UIViewController, UIViewControllerTransitioningDelegate {
     
     let transition = CircleTransition()
+
     
-    @IBOutlet weak var ButtonSkip: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
     
-    @IBAction func skipButt(_ sender: Any) {
+    @IBOutlet weak var subTitle: UILabel!
+    
+    
+    @IBOutlet weak var imagePage1: UIImageView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBAction func startButton(_ sender: Any) {
         performSegue(withIdentifier: "MainSB", sender: self)
         UserDefaults.standard.set(false, forKey: Props.hasBeenSeen)
     }
@@ -25,40 +33,37 @@ class OnViewController: UIViewController, UIViewControllerTransitioningDelegate 
         secondVC.modalPresentationStyle = .custom
     }
     
-    
     func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         transition.transitionMode = .present
-        transition.startingPoint = ButtonSkip.center
+        transition.startingPoint = startButton.center
         transition.circleColor = UIColor(red: 53/255, green: 74/255, blue: 94/255, alpha: 1)
         
         return transition
     }
-
-
+    
+    
     let gradientView:GradientView = GradientView()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let onBoardingBackgroundColor1 = UIColor(red: 26/255, green: 44/255, blue: 59/255, alpha: 1)
         let onBoardingBackgroundColor2 = UIColor(red: 53/255, green: 74/255, blue: 94/255, alpha: 1)
         
         gradientView.frame = CGRect.init(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
         gradientView.FirstColor = onBoardingBackgroundColor1
         gradientView.SecondColor = onBoardingBackgroundColor2
-        self.view.addSubview(gradientView)
-        self.view.addSubview(ButtonSkip)
         
-     
-   }
-    
-    
+        startButton.layer.cornerRadius = 10;
+ 
 
+        self.view.addSubview(gradientView)
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(subTitle)
+        self.view.addSubview(imagePage1)
+        self.view.addSubview(startButton)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
 }
 
