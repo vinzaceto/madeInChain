dcef86488af6580368f8e41659be99bdeec7bdbb
Pasquale Mauriello <pasquale.ebasta@gmail.com>
2018-02-16 23:28:49 +0100

    Redifining OnBoarding

--- Waller/Waller/OnBoarding/OnViewController3.swift
+++ Waller/Waller/OnBoarding/OnViewController3.swift
@@ -17,30 +17,17 @@ class OnViewController3: UIViewController, UIViewControllerTransitioningDelegate
     
     @IBOutlet weak var subTitle: UILabel!
     
+    @IBOutlet weak var bottomLabel: UILabel!
     
     @IBOutlet weak var imagePage1: UIImageView!
     
     @IBOutlet weak var startButton: UIButton!
     
     @IBAction func startButton(_ sender: Any) {
-        performSegue(withIdentifier: "MainSB", sender: self)
+        performSegue(withIdentifier: "segueToMain", sender: self)
         UserDefaults.standard.set(false, forKey: Props.hasBeenSeen)
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let secondVC = segue.destination as! HomeViewController
-        secondVC.transitioningDelegate = self
-        secondVC.modalPresentationStyle = .custom
-    }
-    
-    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        transition.transitionMode = .present
-        transition.startingPoint = startButton.center
-        transition.circleColor = UIColor(red: 53/255, green: 74/255, blue: 94/255, alpha: 1)
-        
-        return transition
-    }
-    
+
     
     let gradientView:GradientView = GradientView()
     
@@ -53,15 +40,12 @@ class OnViewController3: UIViewController, UIViewControllerTransitioningDelegate
         gradientView.frame = CGRect.init(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
         gradientView.FirstColor = onBoardingBackgroundColor1
         gradientView.SecondColor = onBoardingBackgroundColor2
-        
-        startButton.layer.cornerRadius = 10;
- 
 
         self.view.addSubview(gradientView)
         self.view.addSubview(titleLabel)
         self.view.addSubview(subTitle)
         self.view.addSubview(imagePage1)
-        self.view.addSubview(startButton)
+        self.view.addSubview(bottomLabel)
    
 
         
