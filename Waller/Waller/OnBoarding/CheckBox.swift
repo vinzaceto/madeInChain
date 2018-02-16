22188076fa0e8fc421b55937982aeba06f679721
Pasquale Mauriello <pasquale.ebasta@gmail.com>
2018-02-16 14:36:39 +0100

    Restored WalletCell

---
+++ Waller/Waller/OnBoarding/CheckBox.swift
@@ -0,0 +1,20 @@
+//
+//  CheckBox.swift
+//  
+//
+//  Created by Pasquale Mauriello on 16/02/18.
+//
+
+import UIKit
+
+class CheckBox: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
